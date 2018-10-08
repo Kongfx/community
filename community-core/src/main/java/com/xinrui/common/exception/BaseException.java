aa44d6cb89fb43e16bf1473622d9a3b3a1cf37d0
Kongfx <kongfanxin_java@163.com>
2018-10-08 17:39:23 +0800

    community-core

---
+++ community-core/src/main/java/com/xinrui/common/exception/BaseException.java
@@ -0,0 +1,44 @@
+/**
+ *
+ */
+package com.xinrui.common.exception;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.ui.ModelMap;
+
+import com.xinrui.common.support.http.HttpCode;
+
+/**
+ *
+ * @author ShenHuaJie
+ * @version 2016年6月7日 下午8:43:02
+ */
+@SuppressWarnings("serial")
+public abstract class BaseException extends RuntimeException {
+    public BaseException() {
+    }
+
+    public BaseException(Throwable ex) {
+        super(ex);
+    }
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable ex) {
+        super(message, ex);
+    }
+
+    public void handler(ModelMap modelMap) {
+        modelMap.put("code", getCode().value());
+        if (StringUtils.isNotBlank(getMessage())) {
+            modelMap.put("msg", getMessage());
+        } else {
+            modelMap.put("msg", getCode().msg());
+        }
+        modelMap.put("timestamp", System.currentTimeMillis());
+    }
+
+    protected abstract HttpCode getCode();
+}
