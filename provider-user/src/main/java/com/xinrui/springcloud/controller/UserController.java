e15593f1c01934cb934f7d0b448dd5e3af2355d8
Kongfx <kongfanxin_java@163.com>
2018-10-08 18:04:55 +0800

    community update

--- provider-user/src/main/java/com/xinrui/springcloud/controller/UserController.java
+++ provider-user/src/main/java/com/xinrui/springcloud/controller/UserController.java
@@ -1,16 +1,19 @@
 package com.xinrui.springcloud.controller;
 
 import com.xinrui.springcloud.pojo.UserInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @GetMapping("/user/{id}")
     public UserInfo getUserById(@PathVariable Long id){
+        log.info("[/user/{id}]  测试Slf4j是否启用。");
         return new UserInfo(id);
     }
 
--- provider-user/src/main/java/com/xinrui/springcloud/service/IUserService.java
+++ provider-user/src/main/java/com/xinrui/springcloud/service/IUserService.java
@@ -1,10 +1,7 @@
 package com.xinrui.springcloud.service;
 
-import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
 import com.xinrui.springcloud.entity.User;
-
-import java.util.List;
 
 public interface IUserService  extends IService<User> {
 
