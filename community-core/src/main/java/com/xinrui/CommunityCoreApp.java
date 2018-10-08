3b7c0119936bf14b433aff10450a1dc138d9bbd4
Kongfx <kongfanxin_java@163.com>
2018-10-08 17:40:35 +0800

    community-core init

---
+++ community-core/src/main/java/com/xinrui/CommunityCoreApp.java
@@ -0,0 +1,17 @@
+package com.xinrui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class CommunityCoreApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(CommunityCoreApp.class);
+    }
+}
