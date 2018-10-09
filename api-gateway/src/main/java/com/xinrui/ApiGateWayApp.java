6a90225388f0c12afa203295fbd78434d5e06373
Kongfx <kongfanxin_java@163.com>
2018-10-09 10:46:19 +0800

    api-gateway init

---
+++ api-gateway/src/main/java/com/xinrui/ApiGateWayApp.java
@@ -0,0 +1,16 @@
+package com.xinrui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ApiGateWayApp
+{
+    public static void main( String[] args ){
+        SpringApplication.run(ApiGateWayApp.class);
+    }
+}
